d43af053bf0867049feb939e8b2e511217062a42
lucasc896 <chris.lucas@bristol.ac.uk>
2012-12-11 12:27:15 +0100

    add debugging CL option, add MC functionality, add plotDetails

--- Plotting/upgradeRatePlots.py
+++ Plotting/upgradeRatePlots.py
@@ -9,6 +9,20 @@ Copyright (c) 2012 University of Bristol. All rights reserved.
 
 from sys import argv
 from ratePlottingUtils import *
+from optparse import OptionParser
+
+parser = OptionParser()
+
+parser.add_option("-d", "--debug",
+                  action="store_true", dest="doDebug", default=False,
+                  help="run code in Debug mode")
+
+parser.add_option("-m", "--mc",
+						action="store_true", dest="runMC", default=False,
+						help="switch to run over MC samples")
+
+(options, args) = parser.parse_args()
+
 
 
 r.gROOT.SetBatch(True)
@@ -18,135 +32,179 @@ if len(argv)<2:
 	sys.exit()
 
 if argv[1] == "66":
-	inFileName = "../out_files/output_0-10066.root"
-	outFileName = "ratePlots_66PU.pdf"
+	if options.runMC:
+		inFileName = "../rootfiles/output_MC_Neut_Pt_2to20_66PU_v1_PU66.root"
+		outFileName = "plotDump/ratePlots_MC_66PU.pdf"
+	else:
+		inFileName = "../rootfiles/output_0-10066.root"
+		outFileName = "plotDump/ratePlots_66PU.pdf"
 	puScen = 66
 elif argv[1] == "45":
-	inFileName = "../out_files/output_0-10045.root"
-	outFileName = "ratePlots_45PU.pdf"
+	if options.runMC:
+		inFileName = "../rootfiles/output_MC_Neut_Pt_2to20_66PU_v1_PU66.root"
+		outFileName = "plotDump/ratePlots_MC_45PU.pdf"
+	else:
+		inFileName = "../rootfiles/output_0-10045.root"
+		outFileName = "plotDump/ratePlots_45PU.pdf"
 	puScen = 45
+elif argv[1] == "50":
+	inFileName = "../rootfiles/output_MC_Neut_Pt_2to20_PostLS1_v1_short_PU50.root "
+	outFileName = "plotDump/ratePlots_MC_50PU.pdf"
+	puScen = 50
 
-print "\n\n*************************************"
+
+def plotDetails(histKey=""):
+
+	plotVars={
+
+			"Jet":10,
+			"EG":1,
+			"Tau":10,
+			"ETT":20,
+			"ETM":20,
+			"HTT":20,
+			"HTM":20,
+			"Muon":10,
+
+	}
+
+
+print "\n\n","*"*37
 print " > Making Plots for %dPU Scenario < "%(puScen)
-print "*************************************\n"
-
+print "*"*37, "\n"
 
 rFile = r.TFile.Open(inFileName)
-print "* Input File: %s\n"%(inFileName)
+print " >>> Input File: %s\n"%(inFileName)
 
 histList = {
-#		"jets":(["combJets_1_rate",
-#				"combJets_2_rate",
-#				"combJets_3_rate",
-#				"combJets_4_rate",]),
-#
-#		"jetsEr":(["combJetsEr_1_rate",
-#				"combJetsEr_2_rate",
-#				"combJetsEr_3_rate",
-#				"combJetsEr_4_rate",]),
-#
-#		"jetsUp":(["up_towerJet_1_rate",
-#				"up_towerJet_2_rate",
-#				"up_towerJet_3_rate",
-#				"up_towerJet_4_rate",]),
-#
-#		"combEG":(["combEG_1_rate",
-#				"combEG_2_rate",
-#				"combEG_3_rate",
-#				"combEG_4_rate",]),
-#		
-#		"combEGUp":(["up_combEG_1_rate",
+		"jets":([["combJets_1_rate",
+				"combJets_2_rate",
+				"combJets_3_rate",
+				"combJets_4_rate",
+				"combJets_5_rate",
+				"combJets_6_rate",], 70, 5]),
+
+		"jetsEr":([["combJetsEr_1_rate",
+				"combJetsEr_2_rate",
+				"combJetsEr_3_rate",
+				"combJetsEr_4_rate",
+				"combJetsEr_5_rate",
+				"combJetsEr_6_rate"], 70, 5]),
+
+		"combEG":([["combEG_1_rate",
+				"combEG_2_rate",
+				"combEG_3_rate",
+				"combEG_4_rate",], 200, 1]),
+
+		"enSums":([["ETT_rate",
+				"ETM_rate",
+				"HTT_rate",
+				"HTM_rate",], 700, 30]),
+
+		"isoEG":([["isoEG_1_rate",
+				"isoEG_2_rate",
+				"isoEG_3_rate",
+				"isoEG_4_rate",], 200, 3]),
+
+#		"tau":([["tau_1_rate",
+#				"tau_2_rate",], 200]),
+
+		"cenTauJets":([["cenpTauJets_1_rate",
+				"cenpTauJets_2_rate",
+				"cenpTauJets_3_rate",
+				"cenpTauJets_4_rate",], 100, 5]),
+
+		"muonHi":([["muon_hi_1_rate",
+				"muon_hi_2_rate",
+				"muon_hi_3_rate",
+				"muon_hi_4_rate",], 200, 5]),
+
+		"crossTrig1":([["DoubleJetEtar_rate",
+				"SingleMuonEtar_rate",
+				"TauEGCross_rate",
+				"IsoEGCenJet_rate",
+				"IsoEGMET_rate"], 60, 3]),
+
+		"crossTrig2":([["doubleEGCross_rate",
+				"doubleMuCross_rate",
+				"EGMuCross_rate",
+				"MuEGCross_rate",
+				"MuJetCross_rate",], 40, 3]),
+
+#		"combEGUp":([["up_combEG_1_rate",
 #				"up_combEG_2_rate",
 #				"up_combEG_3_rate",
-#				"up_combEG_4_rate",]),
-#
-#		"enSums":(["ETT_rate",
-#				"ETM_rate",
-#				"HTT_rate",
-#				"HTM_rate",]),
-#
-#		"isoEG":(["isoEG_1_rate",
-#			"isoEG_2_rate",
-#			"isoEG_3_rate",
-#			"isoEG_4_rate",]),
-#
-#		"isoEGUp":(["up_isoEm_1_rate",
+#				"up_combEG_4_rate",], 200]),
+
+#		"jetsUp":([["up_towerJet_1_rate",
+#				"up_towerJet_2_rate",
+#				"up_towerJet_3_rate",
+#				"up_towerJet_4_rate",], 200]),
+
+#		"isoEGUp":([["up_isoEm_1_rate",
 #			"up_isoEm_2_rate",
 #			"up_isoEm_3_rate",
-#			"up_isoEm_4_rate"]),
+#			"up_isoEm_4_rate"], 200]),
 #
-#		"nonIsoEGUp":(["up_nonIsoEm_1_rate",
+#		"nonIsoEGUp":([["up_nonIsoEm_1_rate",
 #			"up_nonIsoEm_2_rate",
 #			"up_nonIsoEm_3_rate",
-#			"up_nonIsoEm_4_rate"]),
-#
-#		"tau":(["tau_1_rate",
-#			"tau_2_rate",],),
-#
-#		"isoTauUp":(["up_isoTau_1_rate",
+#			"up_nonIsoEm_4_rate"], 200]),
+
+#		"isoTauUp":([["up_isoTau_1_rate",
 #			"up_isoTau_2_rate",
 #			"up_isoTau_3_rate",
-#			"up_isoTau_4_rate"]),		
+#			"up_isoTau_4_rate"], 200]),		
 #
-#		"nonIsoTauUp":(["up_nonIsoTau_1_rate",
+#		"nonIsoTauUp":([["up_nonIsoTau_1_rate",
 #			"up_nonIsoTau_2_rate",
 #			"up_nonIsoTau_3_rate",
-#			"up_nonIsoTau_4_rate"]),
+#			"up_nonIsoTau_4_rate"], 200]),
 #
-#		"combTauUp":(["up_combTau_1_rate",
+#		"combTauUp":([["up_combTau_1_rate",
 #			"up_combTau_2_rate",
 #			"up_combTau_3_rate",
-#			"up_combTau_4_rate",]),
-##
-##		"muonHi":(["muon_hi_1_rate",
-##			"muon_hi_2_rate",
-##			"muon_hi_3_rate",
-##			"muon_hi_4_rate",]),
+#			"up_combTau_4_rate",], 200]),
+
+#		"jetCompare":([["cenpTauJets_1_rate",
+#			"cenpTauJets_2_rate",
+#			"cenpTauJets_3_rate",
+#			"cenpTauJets_4_rate",],
+#			["up_towerJet_1_rate",
+#			"up_towerJet_2_rate",
+#			"up_towerJet_3_rate",
+#			"up_towerJet_4_rate",], 200]),
 #
-#		"crossTrig1":(["DoubleJetEtar_rate",
-#			"SingleMuonEtar_rate",]),
+#		"isoEGCompare:":([["isoEG_1_rate",
+#			"isoEG_2_rate",
+#			"isoEG_3_rate",
+#			"isoEG_4_rate",],
+#			["up_isoEm_1_rate",
+#			"up_isoEm_2_rate",
+#			"up_isoEm_3_rate",
+#			"up_isoEm_4_rate"],),
 #
-#		"crossTrig2":(["doubleEGCross_rate",
-#			"doubleMuCross_rate",
+#		"crossTrigCompare":([["doubleEGCross_rate",
 #			"EGMuCross_rate",
-#			"MuEGCross_rate",
-#			"MuJetCross_rate",]),
+#			"MuEGCross_rate",],
+#			["up_doubleEGCross_rate",
+#			"up_EGMuCross_rate",
+#			"up_MuEGCross_rate"],),
 #
-		"jetCompare":(["cenpTauJets_1_rate",
-			"cenpTauJets_2_rate",
-			"cenpTauJets_3_rate",
-			"cenpTauJets_4_rate",],
-			["up_towerJet_1_rate",
-			"up_towerJet_2_rate",
-			"up_towerJet_3_rate",
-			"up_towerJet_4_rate",]),
-
-		"isoEGCompare:":(["isoEG_1_rate",
-			"isoEG_2_rate",
-			"isoEG_3_rate",
-			"isoEG_4_rate",],
-			["up_isoEm_1_rate",
-			"up_isoEm_2_rate",
-			"up_isoEm_3_rate",
-			"up_isoEm_4_rate"],),
-
-		"crossTrigCompare":(["doubleEGCross_rate",
-			"EGMuCross_rate",
-			"MuEGCross_rate",],
-			["up_doubleEGCross_rate",
-			"up_EGMuCross_rate",
-			"up_MuEGCross_rate"],),
-
-		"tauCompare":(["tau_1_rate",
-			"tau_2_rate",],
-			["up_combTau_1_rate",
-			"up_combTau_2_rate"],),
+#		"tauCompare":([["tau_1_rate",
+#			"tau_2_rate",],
+#			["up_combTau_1_rate",
+#			"up_combTau_2_rate"],),
 
 }
 
 c1 = Print(outFileName)
 c1.DoPageNum = False
-c1.open("SLHC L1 Trigger Rate Plots - ZBHPF1 @ %sPU"%puScen)
+if options.runMC:
+	label="MC"
+else:
+	label="HPU Data"
+c1.open("SLHC L1 Trigger Rate Plots - %s @ %sPU"%(label, puScen))
 c1.SetGrid(True)
 c1.SetLog('y', True)
 
@@ -155,26 +213,30 @@ for key,hists in sorted(histList.items()):
 	rawUpHist=[]
 	print "\n * Making plot %s *"%(key) 
 
-	if "tuple" in str(type(hists)):
+	if "tuple" in str(type(hists[0])):
 		# contains an norm/upgrade comparison
-		for h, uh in zip(hists[0],hists[1]):
+		for h, uh in zip(hists[0][0],hists[0][1]):
 			rawHist.append(rFile.Get(h))
 			rawUpHist.append(rFile.Get(uh))
 		a = RatePlot(hists=rawHist, upHists=rawUpHist)	
-	elif "list" in str(type(hists)):
+	elif "list" in str(type(hists[0])):
 		# contains no comparison
-		for h in hists:
+		for h in hists[0]:
 			rawHist.append(rFile.Get(h))
 		a = RatePlot(hists=rawHist)
 
-	#a.Debug = True
+	a.Debug = options.doDebug
 	a.PUScen = puScen
+	a.xUpper = hists[1]
+	a.xRebin = hists[2]
+
 
 	c1.PrintCanvas( a.MakeRatePlot() )
 
 	del a
 
 c1.close()
+
 
 # +++ Print Specific Rates +++ #
 
@@ -212,10 +274,13 @@ menu66 = {
 
 if puScen == 45:
 	rateDict = menu45
-	rateFileName = "rateVals_45PU.txt"
+	rateFileName = "txtDump/rateVals_45PU.txt"
 elif puScen == 66:
 	rateDict = menu66
-	rateFileName = "rateVals_66PU.txt"
+	rateFileName = "txtDump/rateVals_66PU.txt"
+elif puScen == 50:
+	rateDict = menu66
+	rateFileName = "txtDump/rateVals_50PU.txt"
 
 textFile = open(rateFileName, 'w')
 
